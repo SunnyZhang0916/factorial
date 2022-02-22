bbb5d7680d0e6c78be3ad07dce967c5fe36d50bb
SunnyZhang0916 <87867258+SunnyZhang0916@users.noreply.github.com>
2022-02-23 00:10:07 +0100

    Basfall: n == 0

---
+++ U6.17.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void print_array (int n, int arr[]);
+
+int main(void)
+{
+  const int n = 7;
+  int a[] = {1, 2, 3, 4, 5, 6, 7};
+
+  print_array (n, a);
+
+  printf("\n");
+
+  return 0;
+}
+
+void print_array (int n, int arr[])
+{
+/* Basfall: n == 0, inget sker i funktionen */
+/* Rekursivt fall */
+  if (n > 0) {
+
+    printf("%d ", arr[n - 1]);
+
+    print_array (n - 1, arr);
+  }
+}
